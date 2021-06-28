f84d4dbaff16fc0d6cab3567a86e6eef4750cb12
masaun <masanoriuno@gmail.com>
2021-06-28 22:05:46 +0900

    [Deployed-addresses on Polygon Mumbai]: Add USDC, DAI, amUSDC, amDAI, pmETH

--- migrations/addressesList/tokenAddress/tokenAddress.js
+++ migrations/addressesList/tokenAddress/tokenAddress.js
@@ -1,8 +1,11 @@
 module.exports = {
     "Polygon Mainnet": {
-        "mUSD": "0xE840B73E5287865EEc17d250bFb1536704B43B21",
-        "imUSD": "0x5290Ad3d83476CA6A2b178Cd9727eE1EF72432af"
     },
     "Polygon Mumbai": {
+        "DAI": "0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F",
+        "amDAI": "0x639cB7b21ee2161DF9c882483C9D55c90c20Ca3e",
+        "USDC": "0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e",
+        "amUSDC": "0x2271e3Fef9e15046d09E1d78a8FF038c691E9Cf9",
+        "pmETH": "0x2f9374157Ef337620b19a720019A6FDB0593d20B"
     }
 }
